be37116f03061ead11e0472655cf1d9f87d2bfa8
jun <junpeeei0407@gmail.com>
2022-07-21 20:36:40 +0900

    テンプレート入力

--- simplewebsite/views.py
+++ simplewebsite/views.py
@@ -3,5 +3,22 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "index.html"
 
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = 'jumpei'
+        return ctxt
+
+    
 class AboutView(TemplateView):
     template_name = "about.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = 'jumpei'
+        ctxt["num_services"] = 123456789
+        ctxt["skills"] = [
+            "shopppingApp",
+            "lineApp",
+            "googlemapApp",        
+        ]
+        return ctxt
